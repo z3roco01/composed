5f16264548cf961543ce4169cf610c536a7a8454
z3roco01 <zerocool@e.email>
2026-05-29 17:41:41 -0500

    support all java 21 versions ( except 26.1.x )

--- src/main/java/z3roco01/composed/Composed.java
+++ src/main/java/z3roco01/composed/Composed.java
@@ -3,6 +3,7 @@ package z3roco01.composed;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import z3roco01.composed.annotation.ConfigProperty;
@@ -17,5 +18,16 @@ public class Composed implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		Config config = new Config();
+        try {
+            ConfigFile.store("./fartttt", config);
+        } catch (IOException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+	public class Config {
+		@ConfigProperty
+		public Item item = Items.ACACIA_LEAVES;
+	}
 }
